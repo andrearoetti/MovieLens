71bbd5ed27e62250275fad784fdbcf71ecd710cb
andrearoetti <roetti@geb.com>
2020-06-17 03:32:56 +0200

    bella botta

--- wrangling.R
+++ wrangling.R
@@ -1,5 +1,77 @@
 library(tidyverse)
-library(lubridate)
-library(dslabs)
-movielens <- data("movielens")
-save(movielens,file="rda/movielens.rda")
+library(lattice)
+library(caret)
+
+edx <- readRDS("C:/Users/roetti/Documents/MovieLens/edx.rds")
+# edx <- readRDS("data/edx.rds")
+validation <- readRDS("C:/Users/roetti/Documents/MovieLens/validation.rds")
+# validation <- readRDS("data/validation.rds")
+
+head(edx)
+
+nrow(edx)
+n_distinct(edx$userId)
+n_distinct(edx$movieId)
+mean(edx$rating)
+
+RMSE <- function(true_ratings, predicted_ratings){
+  sqrt(mean((true_ratings - predicted_ratings)^2))}
+
+set.seed(1, sample.kind = "Rounding")
+test_index <- createDataPartition(y = edx$rating, times = 1,
+                                  p = 0.2, list = FALSE)
+train_set <- edx[-test_index,]
+test_set <- edx[test_index,]
+
+test_set <- test_set %>% 
+  semi_join(train_set, by = "movieId") %>%
+  semi_join(train_set, by = "userId")
+
+mu <- mean(train_set$rating)
+first_rmse <- RMSE(test_set$rating, mu)
+rmse_results <- tibble(method = "Just the average", RMSE = first_rmse)
+rmse_results %>% knitr::kable()
+
+b_i <- train_set %>%
+  group_by(movieId) %>%
+  summarize(b_i = sum(rating - mu)/(n()))
+b_i %>% qplot(b_i, geom ="histogram", bins = 10, data = ., color = I("black"))
+
+second_pred <- test_set %>% 
+  left_join(b_i, by='movieId') %>%
+  mutate(pred = mu + b_i) %>% .$pred
+second_rmse <- RMSE(test_set$rating, second_pred)
+rmse_results <- bind_rows(rmse_results,
+                          tibble(method="Movie Effect Model",  
+                                 RMSE = second_rmse))
+rmse_results %>% knitr::kable()
+
+train_set %>% 
+  group_by(userId) %>% 
+  summarize(b_u = mean(rating)) %>% 
+  filter(n()>=100) %>%
+  ggplot(aes(b_u)) + 
+  geom_histogram(bins = 30, color = "black")
+
+b_u <- train_set %>% 
+  left_join(b_i, by="movieId") %>%
+  group_by(userId) %>%
+  summarize(b_u = sum(rating - b_i - mu)/(n()))
+third_pred <- test_set %>% 
+  left_join(b_i, by='movieId') %>%
+  left_join(b_u, by='userId') %>%
+  mutate(pred = mu + b_i + b_u) %>% .$pred
+third_rmse <- RMSE(test_set$rating, third_pred)
+rmse_results <- bind_rows(rmse_results,
+                          tibble(method="Movie + User Effects Model",  
+                                 RMSE = third_rmse))
+rmse_results %>% knitr::kable()
+
+train_set %>% group_by(genres) %>%
+  summarize(n = n(), avg = mean(rating), se = sd(rating)/sqrt(n())) %>%
+  filter(n >= 1000) %>% 
+  mutate(genres = reorder(genres, avg)) %>%
+  ggplot(aes(x = genres, y = avg, ymin = avg - 2*se, ymax = avg + 2*se)) + 
+  geom_point() +
+  geom_errorbar() + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
